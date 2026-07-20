06d09044083394f6a4c56d7b950f1e86b67bbc84
Shem <mongareshem1@gmail.com>
2026-07-20 16:04:20 +0300

    Change the label type and line thickness

--- generating_data/mpl_squares.py
+++ generating_data/mpl_squares.py
@@ -5,4 +5,13 @@ squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
 ax.plot(squares)
 
+# Set chart title and label axes
+ax.set_title('Square Numbers',
+    fontdict={'family':'Serif', 'fontsize':24, 'fontweight':'bold', 'style':'italic'})
+ax.set_xlabel('Value', fontsize=14)
+ax.set_ylabel('Square of Value', fontsize=14)
+
+# Set size of tick labels
+ax.tick_params(labelsize=14)
+
 plt.show()
