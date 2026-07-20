d053822f0b9332b14c225f353cc3314fa7031041
Shem <mongareshem1@gmail.com>
2026-07-20 17:11:37 +0300

    Use a color map

--- generating_data/scatter_squares.py
+++ generating_data/scatter_squares.py
@@ -8,7 +8,8 @@ plt.style.use('seaborn-v0_8-darkgrid')
 fig, ax = plt.subplots()
 
 # ax.scatter(x_values, y_values, color='turquoise', s=5)
-ax.scatter(x_values, y_values, color=(0.8, 0, 0), s=10)
+# ax.scatter(x_values, y_values, color=(0.8, 0, 0), s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.rainbow, s=10)
 
 ax.set_title('Squares', fontsize=30, fontweight='bold')
 ax.set_xlabel('Value', fontsize=16)
