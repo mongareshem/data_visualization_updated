9c94f8957201d7d427f97f182975b0faf76959f5
Shem <mongareshem1@gmail.com>
2026-07-20 18:26:20 +0300

    Emphasize the first and last points

--- generating_data/rw_visual.py
+++ generating_data/rw_visual.py
@@ -15,6 +15,10 @@ while True:
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=10)
     ax.set_aspect('equal')
 
+    # Emphasize the first and last points.
+    ax.scatter(0, 0, color='green', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
     plt.show()
 
     keep_running = input('Make another walk? (y/n): ')
