f0102ca66d3688775aab218c4753cd73d295daa6
Shem <mongareshem1@gmail.com>
2026-07-20 16:49:35 +0300

    Calculate data automatically

--- generating_data/scatter_squares.py
+++ generating_data/scatter_squares.py
@@ -1,18 +1,22 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1000)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8-darkgrid')
 
 fig, ax = plt.subplots()
 #ax.scatter(2, 4, s=200)
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, s=5)
 
 ax.set_title('Squares', fontsize=30, fontweight='bold')
 ax.set_xlabel('Value', fontsize=16)
 ax.set_ylabel('Square of Value', fontsize=16)
 
 ax.tick_params(labelsize=14)
+
+ax.set_xlim(0, 1100)
+ax.set_ylim(0, 1_100_000)
+# ax.axis([0, 1000, 0, 1_100_000]) #type: ignore
 
 plt.show()
