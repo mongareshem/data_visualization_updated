1d016fa9d2e010de3a0e1f8c1da4e8de5ef6a55d
Shem <mongareshem1@gmail.com>
2026-07-20 16:34:54 +0300

    Style an individual point

--- generating_data/scatter_squares.py
+++ generating_data/scatter_squares.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn-v0_8-darkgrid')
 
 fig, ax = plt.subplots()
-ax.scatter(2, 4)
+ax.scatter(2, 4, s=200)
+
+ax.set_title('Squares', fontsize=30, fontweight='bold')
+ax.set_xlabel('Value', fontsize=16)
+ax.set_ylabel('Square of Value', fontsize=16)
+
+ax.tick_params(labelsize=14)
 
 plt.show()
