71448b74834b8564f67444c619b4e212f609f199
Shem <mongareshem1@gmail.com>
2026-07-20 16:39:18 +0300

    Plot a series of point using scatter()

--- generating_data/scatter_squares.py
+++ generating_data/scatter_squares.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
 plt.style.use('seaborn-v0_8-darkgrid')
 
 fig, ax = plt.subplots()
-ax.scatter(2, 4, s=200)
+#ax.scatter(2, 4, s=200)
+ax.scatter(x_values, y_values, s=100)
 
 ax.set_title('Squares', fontsize=30, fontweight='bold')
 ax.set_xlabel('Value', fontsize=16)
