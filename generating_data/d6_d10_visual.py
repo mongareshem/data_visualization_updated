60468f4915595f5b9cefb590e010f2321268358d
Shem <mongareshem1@gmail.com>
2026-07-21 05:01:16 +0300

    Roll dice of different sizes

---
+++ generating_data/d6_d10_visual.py
@@ -0,0 +1,17 @@
+import plotly.express as px
+
+from die import Die
+
+die_1 = Die()
+die_2 = Die(10)
+
+results = [die_1.roll() + die_2.roll() for i in range(50_000)]
+
+poss_results = range(2, die_1.num_sides + die_2.num_sides + 1)
+frequencies = [results.count(result) for result in poss_results]
+
+title = 'Result of rolling a D6 and a D10 50,000 times'
+labels = {'x': 'Result', 'y': 'Frequency of the Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.update_layout(title={'x':0.5}, xaxis_dtick=1)
+fig.show()
