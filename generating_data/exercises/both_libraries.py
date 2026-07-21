5e28fd7fcbac34d2da63dabc3af93e62ec6aa454
Shem <mongareshem1@gmail.com>
2026-07-21 15:20:00 +0300

    Create visualizations of rolling dice using matplotlib.pyplot

---
+++ generating_data/exercises/both_libraries.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+from generating_data.die import Die
+
+die_1 = Die()
+die_2 = Die()
+die_3 = Die()
+
+results = [die_1.roll() + die_2.roll() + die_3.roll() for i in range(100_000)]
+
+poss_results = range(3, die_1.num_sides + die_2.num_sides + die_3.num_sides + 1)
+frequencies = [results.count(value) for value in poss_results]
+
+fig, ax = plt.subplots()
+ax.scatter(poss_results, frequencies)
+
+plt.show()
