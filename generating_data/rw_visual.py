50b90b2017052ac35e3a3fe945a3a5ed31b03868
Shem <mongareshem1@gmail.com>
2026-07-21 12:51:20 +0300

    Replace scatter with plot and reduce points

--- generating_data/rw_visual.py
+++ generating_data/rw_visual.py
@@ -4,7 +4,7 @@ from random_walk import RandomWalk
 
 while True:
     # Make a random walk
-    rw = RandomWalk(50_000)
+    rw = RandomWalk(5_000)
     rw.fill_walk()
 
     # Plot the points in the walk
@@ -12,12 +12,12 @@ while True:
     fig, ax = plt.subplots(figsize=(14,8)) # figsize=(14,8), dpi=128 # default dpi=100
 
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
+    ax.plot(rw.x_values, rw.y_values, color='maroon', linewidth=2)
     ax.set_aspect('equal')
 
     # Emphasize the first and last points.
-    ax.scatter(0, 0, color='green', edgecolors='none', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    ax.scatter(0, 0, color='green', edgecolors='none', s=200)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='blue', edgecolors='none', s=200)
 
     # Remove the axes
     ax.get_xaxis().set_visible(False)
