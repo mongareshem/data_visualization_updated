d9379f23c014b42c6e956968f00440c6024c5d21
Shem <mongareshem1@gmail.com>
2026-07-22 12:19:49 +0300

    Make a list of all earthquakes

--- downloading_data/eq_explore_data.py
+++ downloading_data/eq_explore_data.py
@@ -10,3 +10,7 @@ all_eq_data = json.loads(contents)
 path = Path('eq_data/readable_eq_data.geojson')
 readable_contents = json.dumps(all_eq_data, indent=4)
 path.write_text(readable_contents)
+
+# Examine all the earthquakes in the dataset
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
