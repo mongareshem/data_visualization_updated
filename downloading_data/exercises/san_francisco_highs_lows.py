c062f0b0b9b5c27696527c1c46bb09b0e4440dcf
Shem <mongareshem1@gmail.com>
2026-07-23 02:25:36 +0300

    Get the data for dates, maximum and minimum temperatures and store them in lists

--- downloading_data/exercises/san_francisco_highs_lows.py
+++ downloading_data/exercises/san_francisco_highs_lows.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import csv
+from datetime import datetime
 
 path = Path('../weather_data/san_francisco_2025_full.csv')
 lines = path.read_text().splitlines()
@@ -9,3 +10,12 @@ header_row = next(reader)
 
 for i, column_header in enumerate(header_row):
     print(i, column_header)
+
+dates, t_maxs, t_mins, = [], [], [],
+for row in reader:
+    dates.append(datetime.strptime(row[5], '%Y-%m-%d'))
+    try:
+        t_maxs.append(int(row[8]))
+        t_mins.append(int(row[9]))
+    except ValueError:
+        print(f'Missing data for {row[5]}')
