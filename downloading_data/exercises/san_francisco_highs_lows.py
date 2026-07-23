6f70be04a397463ddab8ff5e58923cbcb347d054
Shem <mongareshem1@gmail.com>
2026-07-23 03:21:21 +0300

    Style the plots

--- downloading_data/exercises/san_francisco_highs_lows.py
+++ downloading_data/exercises/san_francisco_highs_lows.py
@@ -14,9 +14,10 @@ header_row = next(reader)
 for i, column_header in enumerate(header_row):
     print(i, column_header)
 
-dates, t_maxs, t_mins, = [], [], [],
+dates, t_maxs, t_mins, place = [], [], [], ''
 for row in reader:
     dates.append(datetime.strptime(row[5], '%Y-%m-%d'))
+    place = row[1]
     try:
         t_maxs.append(int(row[8]))
         t_mins.append(int(row[9]))
@@ -31,5 +32,12 @@ ax.plot(dates, t_maxs, color='gold')
 ax.plot(dates, t_mins, color='darkmagenta')
 ax.fill_between(dates, t_maxs, t_mins, color='midnightblue', alpha=0.5)
 
+ax.set_title(f'2025 Temperature Readings for \n {place}',
+             fontdict={'family':'Serif', 'size':18,})
+ax.set_ylabel('Temperature (F)', fontdict={'family':'Serif', 'fontsize':18})
+ax.tick_params(labelsize=12)
+
 plt.tight_layout()
+
+plt.savefig('san_francisco_temperatures_2025.png', dpi=300,) # transparent=True
 plt.show()
