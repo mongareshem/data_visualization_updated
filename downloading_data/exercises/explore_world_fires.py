3dba246aec218f65227c424c850ca3feabb99e7b
Shem <mongareshem1@gmail.com>
2026-07-23 23:24:46 +0300

    Get longitudes, latitudes and brightness from the file and create a world map

--- downloading_data/exercises/explore_world_fires.py
+++ downloading_data/exercises/explore_world_fires.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import csv
 
+import plotly.express as px
+
 path = Path('../eq_data/world_fires_1_day.csv')
 lines = path.read_text().splitlines()
 
@@ -9,3 +11,16 @@ header_row = next(reader)
 
 for i, v in enumerate(header_row):
     print(i, v)
+
+lons, lats, brightness, = [], [], []
+for column_header in reader:
+    lons.append(column_header[1])
+    lats.append(column_header[0])
+    brightness.append(column_header[2])
+
+print(lons[:3])
+print(lats[:3])
+print(brightness[:3])
+
+fig = px.scatter_geo(lon=lons, lat=lats)
+fig.show()
