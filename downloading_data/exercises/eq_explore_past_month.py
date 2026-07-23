75fb51a980334b773b2d7aaff043d009be4cb5af
Shem <mongareshem1@gmail.com>
2026-07-23 15:32:09 +0300

    Create a visualization of the earthquakes and style it

--- downloading_data/exercises/eq_explore_past_month.py
+++ downloading_data/exercises/eq_explore_past_month.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import json
 
+import plotly.express as px
+
 path = Path('../eq_data/past_month_eq_data.geojson')
 content = path.read_text()
 all_eq_data = json.loads(content)
@@ -12,14 +14,32 @@ path.write_text(readable_content)
 print(type(all_eq_data)) # dictionary
 # print(type(readable_content)) # string
 
-lons, lats, elevs, mags, = [], [], [], []
+lons, lats, mags, hover_names = [], [], [], [],
 for feature in all_eq_data['features']:
     lons.append(feature['geometry']['coordinates'][0])
     lats.append(feature['geometry']['coordinates'][1])
-    elevs.append(feature['geometry']['coordinates'][2])
     mags.append(feature['properties']['mag'])
+    hover_names.append(feature['properties']['title'])
 
 print(lons[:3])
 print(lats[:3])
-print(elevs[:3])
 print(mags[:3])
+
+fig = px.scatter_geo(lon=lons, lat=lats, size=mags,
+                     color=mags,
+                     color_continuous_scale='inferno_r', # _r at the end reverses
+                     labels={'color':'magnitude', 'lon':'longitude'},
+                     projection='natural earth',
+                     hover_name=hover_names,
+
+                     )
+
+fig.update_layout(title={
+                         'text':all_eq_data['metadata']['title'],
+                         'x':0.5,
+                         'font':{
+                             'family':'Serif',
+                             'size': 23
+                         }}, )
+
+fig.show()
