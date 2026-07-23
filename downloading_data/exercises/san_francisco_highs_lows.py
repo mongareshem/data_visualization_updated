e6d3232cf4ed414082663ce6303c0bddf48249b4
Shem <mongareshem1@gmail.com>
2026-07-23 03:00:28 +0300

    Plot the graphs for the maximum and minimum temperatures

--- downloading_data/exercises/san_francisco_highs_lows.py
+++ downloading_data/exercises/san_francisco_highs_lows.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import csv
 from datetime import datetime
 
+import matplotlib.pyplot as plt
+from numpy.ma.extras import average
+
 path = Path('../weather_data/san_francisco_2025_full.csv')
 lines = path.read_text().splitlines()
 
@@ -19,3 +22,14 @@ for row in reader:
         t_mins.append(int(row[9]))
     except ValueError:
         print(f'Missing data for {row[5]}')
+        t_maxs.append(round(average(t_maxs)))
+        t_mins.append(round(average(t_mins)))
+
+plt.style.use('dark_background')
+fig, ax = plt.subplots()
+ax.plot(dates, t_maxs, color='gold')
+ax.plot(dates, t_mins, color='darkmagenta')
+ax.fill_between(dates, t_maxs, t_mins, color='midnightblue', alpha=0.5)
+
+plt.tight_layout()
+plt.show()
