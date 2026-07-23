ab6220162417bc955faf64197208c0e1f7cbebee
Shem <mongareshem1@gmail.com>
2026-07-23 23:48:09 +0300

    Add and style the title

--- downloading_data/exercises/explore_world_fires.py
+++ downloading_data/exercises/explore_world_fires.py
@@ -16,11 +16,22 @@ lons, lats, brightness, = [], [], []
 for column_header in reader:
     lons.append(column_header[1])
     lats.append(column_header[0])
-    brightness.append(column_header[2])
+    brightness.append(float(column_header[2]))
 
 print(lons[:3])
 print(lats[:3])
 print(brightness[:3])
 
-fig = px.scatter_geo(lon=lons, lat=lats)
+title = 'World Fires'
+fig = px.scatter_geo(lon=lons, lat=lats, size=brightness,
+                     title=title,
+                     projection='natural earth')
+
+fig.update_layout(title={'text':title,
+                         'x':0.5,
+                         'font':{
+                             'family':'Serif',
+                             'size': 44
+                         }})
+
 fig.show()
