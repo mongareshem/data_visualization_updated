c006e1d6468d361ddcc4846b9147a4e6fcf7d4db
Shem <mongareshem1@gmail.com>
2026-07-23 14:47:36 +0300

    Get the lons, lats, elevs, mags from the geo json file

--- downloading_data/exercises/eq_explore_past_month.py
+++ downloading_data/exercises/eq_explore_past_month.py
@@ -8,3 +8,18 @@ all_eq_data = json.loads(content)
 path = Path('../eq_data/readable_past_month_eq_data.json')
 readable_content = json.dumps(all_eq_data, indent=4)
 path.write_text(readable_content)
+
+print(type(all_eq_data)) # dictionary
+# print(type(readable_content)) # string
+
+lons, lats, elevs, mags, = [], [], [], []
+for feature in all_eq_data['features']:
+    lons.append(feature['geometry']['coordinates'][0])
+    lats.append(feature['geometry']['coordinates'][1])
+    elevs.append(feature['geometry']['coordinates'][2])
+    mags.append(feature['properties']['mag'])
+
+print(lons[:3])
+print(lats[:3])
+print(elevs[:3])
+print(mags[:3])
--- experimental.py
+++ experimental.py
@@ -5,3 +5,16 @@ print(header.index('TMAX')) # First occurrence
 nums = [12, 25, 12, 44, 33, 12, 56]
 indices = [i for i, x in enumerate(nums) if x==12] # All instances
 print(indices)
+
+geometry =  {
+    "type": "Point",
+    "coordinates": [
+        -75.3034,
+        -12.1036,
+        10
+    ]
+}
+for i, v in enumerate(geometry):
+    print(i, v)
+
+print(geometry['coordinates'])
