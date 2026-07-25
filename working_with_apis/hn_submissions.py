35699e9492ad217a32562137d4515d66c5174c43
Shem <mongareshem1@gmail.com>
2026-07-25 23:15:42 +0300

    Make an API call and check its response

---
+++ working_with_apis/hn_submissions.py
@@ -0,0 +1,8 @@
+from operator import itemgetter
+
+import requests
+
+# Make an API call and check the response
+url =  "https://hacker-news.firebaseio.com/v0/topstories.json"
+r = requests.get(url)
+print(f'Status Code: {r.status_code}')
