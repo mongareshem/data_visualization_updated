437b89240a1b11c036594b0292bdfe38a7aa869e
Shem <mongareshem1@gmail.com>
2026-07-25 23:39:58 +0300

    Create a dictionary for each submission dictionary

--- working_with_apis/hn_submissions.py
+++ working_with_apis/hn_submissions.py
@@ -12,10 +12,20 @@ submission_ids = r.json() # Returns a list
 # print(submission_ids)
 
 submission_dicts = []
-for submission_id in submission_ids[:5]:
+for submission_id in submission_ids[:2]:
     # Make a new API call for each submission
     url = f'https://hacker-news.firebaseio.com/v0/item/{submission_id}.json'
     r = requests.get(url)
     print(f'Status Code: {r.status_code}')
     response_dict = r.json() # dictionary
-    # print(response_dict)
+    print(response_dict)
+
+    # Build a dictionary for each article
+    submission_dict = {
+        'title': response_dict['title'],
+        'hn_link': response_dict['url'],
+        'comments': response_dict['descendants']
+    }
+    submission_dicts.append(submission_dict)
+
+print(submission_dicts)
