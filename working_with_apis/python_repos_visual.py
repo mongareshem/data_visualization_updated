0baf3431d6a05eef1137d0a5c1928556b4108c0a
Shem <mongareshem1@gmail.com>
2026-07-25 14:14:07 +0300

    Add clickable links

--- working_with_apis/python_repos_visual.py
+++ working_with_apis/python_repos_visual.py
@@ -19,9 +19,14 @@ print(f'Complete Results: {not response_dict['incomplete_results']}')
 
 # Process repository information
 repo_dicts = response_dict['items']
-repo_names, stars, hover_texts = [], [], []
+repo_links, stars, hover_texts = [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
+    # Turn repo names into active links
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f'<a href={repo_url}>{repo_name}</a>'
+    repo_links.append(repo_link)
+
     stars.append(repo_dict['stargazers_count'])
 
     # Build hover texts.
@@ -30,17 +35,18 @@ for repo_dict in repo_dicts:
     hover_text = f'{owner}<br />{description}' # <br /> is a line break
     hover_texts.append(hover_text)
 
-
-print(repo_names[:5])
-print(stars[:5])
-
 # Make visualization
 title = 'Most Starred Python Projects on GitHub '
 labels = {'x': 'Repository', 'y': 'Stars'}
-fig = px.bar(x=repo_names, y=stars, title=title,
+fig = px.bar(x=repo_links, y=stars, title=title,
              labels=labels, hover_name=hover_texts)
 
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20,
                   yaxis_title_font_size=20)
 
 fig.show()
+
+# Note that plotly does not support hyperlinks.
+# Use Bokeh or Dash for that
+# Simpler HTML formats like <br>, <sup></sup>, <sub></sub>, <i></i>,
+# <b></b>, <s></s>, <u></u>
