49439de110d27b53476acb956943e88acb1e5ee7
Shem <mongareshem1@gmail.com>
2026-07-25 10:00:10 +0300

    Add comments to the code

--- working_with_apis/python_repos.py
+++ working_with_apis/python_repos.py
@@ -1,5 +1,6 @@
 import requests
 
+# Make an API call and check the responses
 url = "https://api.github.com/search/repositories?q=language:python+sort:stars"
 headers = {
     "Accept": "application/json",              # JSON response
@@ -9,5 +10,8 @@ headers = {
 r = requests.get(url, headers=headers)
 print(f'Status Code: {r.status_code}')
 
+# Convert the response to a dictionary
 response_dict = r.json()
+
+# Process Results
 print(response_dict.keys())
