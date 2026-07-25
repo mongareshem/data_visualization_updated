6cf621e5629ae11cd109b4f93707f36775cf98d0
Shem <mongareshem1@gmail.com>
2026-07-25 23:52:26 +0300

    Print the title, link and number of comments for each article

--- working_with_apis/hn_submissions.py
+++ working_with_apis/hn_submissions.py
@@ -18,7 +18,7 @@ for submission_id in submission_ids[:2]:
     r = requests.get(url)
     print(f'Status Code: {r.status_code}')
     response_dict = r.json() # dictionary
-    print(response_dict)
+    # print(response_dict)
 
     # Build a dictionary for each article
     submission_dict = {
@@ -31,3 +31,8 @@ for submission_id in submission_ids[:2]:
 # print(submission_dicts)
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
                           reverse=True)
+
+for submission_dict in submission_dicts:
+    print(f'\nTitle: {submission_dict['title']}')
+    print(f'Discussion Link: {submission_dict['hn_link']}')
+    print(f'Comments: {submission_dict['comments']}')
