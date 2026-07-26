13aca1a9ac1a0a8ec9c5a94f3d1ad03cb16d796c
Shem <mongareshem1@gmail.com>
2026-07-27 02:20:18 +0300

    Store the response in a json file

--- working_with_apis/exercises/javascript_repos.py
+++ working_with_apis/exercises/javascript_repos.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+import json
+
 import requests
 
 url = ("https://api.github.com/search/repositories"
@@ -5,3 +8,7 @@ url = ("https://api.github.com/search/repositories"
 r = requests.get(url)
 print(f'Status Code: {r.status_code}')
 
+response_dict = r.json()
+path = Path('./javascript_repos.json')
+contents = json.dumps(response_dict, indent=4)
+path.write_text(contents)
