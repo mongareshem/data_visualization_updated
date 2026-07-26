e142627105e539224d9c8a3209e7ac63615fd6ec
Shem <mongareshem1@gmail.com>
2026-07-27 02:30:34 +0300

    Print keys and their values

--- working_with_apis/exercises/javascript_repos.py
+++ working_with_apis/exercises/javascript_repos.py
@@ -8,7 +8,13 @@ url = ("https://api.github.com/search/repositories"
 r = requests.get(url)
 print(f'Status Code: {r.status_code}')
 
-response_dict = r.json()
+response_dict = r.json() # Dictionary
 path = Path('./javascript_repos.json')
 contents = json.dumps(response_dict, indent=4)
 path.write_text(contents)
+
+for key in response_dict.keys():
+       print(key)
+
+print(f'\nTotal Count: {response_dict['total_count']}')
+print(f'Complete Results: {not response_dict['incomplete_results']}')
