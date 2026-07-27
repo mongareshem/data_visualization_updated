05dcd4997349ba8de6e3b830b90b30075f87f950
Shem <mongareshem1@gmail.com>
2026-07-27 17:06:19 +0300

    Add a couple other tests

--- working_with_apis/exercises/test_python_repos.py
+++ working_with_apis/exercises/test_python_repos.py
@@ -1,6 +1,6 @@
-from working_with_apis.python_repos import r
-
-# print(r.status_code)
+from working_with_apis.python_repos import r, response_dict
 
 def test_successful_api_call():
     assert r.status_code == 200
+    assert response_dict['total_count'] == 971
+    assert len(response_dict['items']) == 30
